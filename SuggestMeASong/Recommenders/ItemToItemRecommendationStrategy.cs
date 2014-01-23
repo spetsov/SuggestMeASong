4fc2816d82b4489f42830146e04478fb5512c174
spetsov <spsetsov@gmail.com>
2014-01-23 21:21:11 +0200

    Styles and 2 new strategies

---
+++ SuggestMeASong/Recommenders/ItemToItemRecommendationStrategy.cs
@@ -0,0 +1,232 @@
+﻿using SuggestMeASong.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SuggestMeASong.Recommenders
+{
+    public class ItemToItemRecommendationStrategy : IRecommendationStrategy
+    {
+        private string externalProviderName;
+        private const int K = 3;
+
+        public ItemToItemRecommendationStrategy(string externalProviderName)
+        {
+            this.externalProviderName = externalProviderName;
+        }
+        public async Task<List<Rating>> Recommend(int userId)
+        {
+            Dictionary<string, List<Rating>> allRatedItems = GetAllRatedItems(externalProviderName);
+
+
+            Dictionary<int, List<Rating>> currentUserAllRatings = GetAllRatings(externalProviderName);
+
+
+            //This is the item we will test against
+            string currentUserTopRatedItemId = GetOneOfTopRatedItems(userId, currentUserAllRatings);
+
+
+            //if no items return
+            if (currentUserTopRatedItemId == null)
+            {
+                return new List<Rating>();
+            }
+
+            List<Rating> result = DoItemBasedFiltering(currentUserTopRatedItemId, allRatedItems);
+
+            //List<Rating> recommendations = new List<Rating>();
+
+            return result;
+        }
+
+        private List<Rating> DoItemBasedFiltering(string currentItem, Dictionary<string, List<Rating>> allRatedItems)
+        {
+            List<Rating> currentItemVector = allRatedItems[currentItem];
+  
+            Dictionary<string, double> allSimilarities = new Dictionary<string, double>();
+            foreach (var element in allRatedItems)
+            {
+                if (element.Key != currentItem)
+                {
+                    List<Rating> otherItemVector = element.Value;
+                    
+                    var similarity = CalculateCosineSimilarity(currentItemVector, otherItemVector); 
+                    
+                    
+                    if (similarity > 0.7)
+                    {
+                        allSimilarities.Add(element.Key, similarity);
+                    }
+                }
+            }
+
+
+            List<Rating> result = GetTopKRatings(K, allSimilarities, allRatedItems);
+            return result;
+        }
+
+        private List<Rating> GetTopKRatings(int k, Dictionary<string, double> similarities, Dictionary<string, List<Rating>> ratedItems)
+        {
+            if (similarities.Count >= k && ratedItems.Count >= k)
+            {
+                List<Rating> result = new List<Rating>();
+
+                similarities = similarities.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            
+                int counter = 0;
+                foreach (var pair in similarities)
+                {
+                    if (counter < k)
+                    {
+                        Rating itemRating = new Rating();
+
+                        itemRating.ExternalId = pair.Key;
+                        result.Add(itemRating);
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    counter++;
+                }
+
+                return result;
+            }
+
+            return new List<Rating>();
+        }
+
+
+        private string GetOneOfTopRatedItems(int userId, Dictionary<int, List<Rating>> allRatings)
+        {
+            if (!allRatings.ContainsKey(userId))
+            {
+                return null;
+            }
+
+            List<Rating> currentUserRatings = allRatings[userId];     
+
+            int topRating = 0;
+
+            foreach (var rating in currentUserRatings)
+            {
+                if (rating.Value > topRating)
+                {
+                    topRating = rating.Value;
+                }
+            }
+
+
+            
+            List<Rating> topRatings = new List<Rating>();
+
+            foreach (var rating in currentUserRatings)
+            {
+                if (rating.Value == topRating)
+                {
+                    topRatings.Add(rating);
+                }
+            }
+            Random rnd = new Random();
+            int randIndex = rnd.Next(topRatings.Count);
+            Rating result = topRatings[randIndex];
+
+            return result.ExternalId;
+        }
+
+
+        private Dictionary<string, List<Rating>> GetAllRatedItems(string externalProviderName)
+        {
+            using (SongsContext cntx = new SongsContext())
+            {
+                
+                var ratings = cntx.Ratings.Where(r => r.ExternalProviderName == this.externalProviderName).ToList();
+                Dictionary<string, List<Rating>> dict = new Dictionary<string, List<Rating>>();
+                foreach (var rating in ratings)
+                {
+
+                    if (!dict.ContainsKey(rating.ExternalId))
+                    {
+                        dict.Add(rating.ExternalId, new List<Rating>());
+                    }
+                    dict[rating.ExternalId].Add(rating);
+                }
+                return dict;
+            }
+        }
+
+        private Dictionary<int, List<Rating>> GetAllRatings(string externalProviderName)
+        {
+            using (SongsContext cntx = new SongsContext())
+            {
+                var ratings = cntx.Ratings.Where(r => r.ExternalProviderName == this.externalProviderName).ToList();
+                Dictionary<int, List<Rating>> dict = new Dictionary<int, List<Rating>>();
+                foreach (var rating in ratings)
+                {
+                    if (!dict.ContainsKey(rating.UserId))
+                    {
+                        dict.Add(rating.UserId, new List<Rating>());
+                    }
+                    dict[rating.UserId].Add(rating);
+                }
+                return dict;
+            }
+        }
+
+
+        private double CalculateCosineSimilarity(List<Rating> vector1, List<Rating> vector2)
+        {
+  
+            if (vector1.Count < vector2.Count)
+            {
+                int difference = vector2.Count - vector1.Count;
+
+                for (int i = 0; i < difference; ++i)
+                {
+                    Rating rat = new Rating();
+                    vector1.Add(rat);
+                }
+            }
+            if (vector1.Count > vector2.Count)
+            {
+                int difference = vector1.Count - vector2.Count;
+
+                for (int i = 0; i < difference; ++i)
+                {
+                    Rating rat = new Rating();
+                    vector2.Add(rat);
+                }
+            }
+
+            double vector1Length = GetVectorLength(vector1);
+            double vector2Length = GetVectorLength(vector2);
+
+            double sum = 0.0;
+
+            for (int i = 0; i < vector1.Count; ++i )
+            {
+                sum += vector1[i].Value * vector2[i].Value;
+            }
+
+            sum /= (vector1Length * vector2Length);
+
+            return sum;
+           
+        }
+
+        private double GetVectorLength(List<Rating> ratings)
+        {
+            double sum = 0;
+            foreach (var rating in ratings)
+            {
+                sum += Math.Pow(rating.Value, 2);
+            }
+
+            return Math.Sqrt(sum);
+        }
+    }
+}
---
+++ SuggestMeASong/Recommenders/UserToUserAdjustedCosineRecommendationStrategy.cs
@@ -0,0 +1,134 @@
+﻿using SuggestMeASong.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SuggestMeASong.Recommenders
+{
+    public class UserToUserAdjustedCosineRecommendationStrategy : IRecommendationStrategy
+    {
+        private string externalProviderName;
+        private const int K = 3;
+
+        public UserToUserAdjustedCosineRecommendationStrategy(string externalProviderName)
+        {
+            this.externalProviderName = externalProviderName;
+        }
+        public async Task<List<Rating>> Recommend(int userId)
+        {
+            Dictionary<int, List<Rating>> allRatings = GetAllRatings(externalProviderName);
+            if (!allRatings.ContainsKey(userId))
+            {
+                return new List<Rating>();
+            }
+            return GetAllSuggestionsOfKNearestUsers(K, userId, allRatings).Where(r => r.Value >= 3).ToList();
+        }
+
+        private List<Rating> GetAllSuggestionsOfKNearestUsers(int k, int userId, Dictionary<int, List<Rating>> allRatings)
+        {
+            var currentUserRatings = allRatings[userId];
+            double currentUserAverageRating = CalculateAverageRating(currentUserRatings);
+            for (int i = 0; i < currentUserRatings.Count; ++i )
+            {
+               
+                currentUserRatings[i].Value -= (int)currentUserAverageRating;
+            }
+
+
+
+            var currentUserVectorLength = this.GetVectorLength(currentUserRatings);
+            Dictionary<int, double> allSimilarities = new Dictionary<int, double>();
+            foreach (var rating in allRatings)
+            {
+                if (rating.Key != userId)
+                {
+                    var similarity = this.CalculateAdjustedCosineSimilarity(userId, currentUserVectorLength,
+                        currentUserRatings, rating.Key, allRatings);
+                    if (similarity >= 0.2)
+                    {
+                        allSimilarities.Add(rating.Key, similarity);
+                    }
+                }
+            }
+            var nearestUserIds = allSimilarities.OrderByDescending(i => i.Value).Take(k);
+            List<Rating> result = new List<Rating>();
+            foreach (var pair in nearestUserIds)
+            {
+                result.AddRange(allRatings[pair.Key].Where(r => !currentUserRatings.Any(i => i.ExternalId == r.ExternalId)));
+            }
+            return result;
+        }
+
+        private Dictionary<int, List<Rating>> GetAllRatings(string externalProviderName)
+        {
+            using (SongsContext cntx = new SongsContext())
+            {
+                
+                var ratings = cntx.Ratings.Where(r => r.ExternalProviderName == this.externalProviderName).ToList();
+                Dictionary<int, List<Rating>> dict = new Dictionary<int, List<Rating>>();
+                foreach (var rating in ratings)
+                {
+                    if (!dict.ContainsKey(rating.UserId))
+                    {
+                        dict.Add(rating.UserId, new List<Rating>());
+                    }
+                    dict[rating.UserId].Add(rating);
+                }
+                return dict;
+            }
+        }
+
+
+        private double CalculateAverageRating(List<Rating> ratings)
+        {
+            double averageRating = 0.0;
+            foreach (var rating in ratings)
+            {
+                averageRating += rating.Value;
+            }
+
+            averageRating /= ratings.Count;
+
+            return averageRating;
+        }
+
+        private double CalculateAdjustedCosineSimilarity(int userId, double currentUserVectorLength, List<Rating> currentUserRatings,
+            int otherUserId, Dictionary<int, List<Rating>> allRatings)
+        {
+            List<Rating> otherUserRatings = allRatings[otherUserId];
+            double otherUserAverageRating = CalculateAverageRating(otherUserRatings);
+            for (int i = 0; i < otherUserRatings.Count; ++i)
+            {
+                otherUserRatings[i].Value -= (int)otherUserAverageRating;
+            }
+            double otherUserVectorLength = this.GetVectorLength(otherUserRatings);
+            
+            int sum = 0;
+            foreach (var rating in currentUserRatings)
+            {
+                var otherUserRating = otherUserRatings.Where(r => r.ExternalId == rating.ExternalId).FirstOrDefault();
+                int otherUserRatingValue = 0;
+                if (otherUserRating != null)
+                {
+                    otherUserRatingValue = otherUserRating.Value;
+                }
+                sum += rating.Value * otherUserRatingValue;
+            }
+
+            return sum / (currentUserVectorLength * otherUserVectorLength);
+        }
+
+        private double GetVectorLength(List<Rating> ratings)
+        {
+            double sum = 0;
+            foreach (var rating in ratings)
+            {
+                sum += Math.Pow(rating.Value, 2);
+            }
+
+            return Math.Sqrt(sum);
+        }
+    }
+}
